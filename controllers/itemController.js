b51a9f6c423eac70d27d8bbe5ba8e9ede2822ccb
pgarbrecht <philgarbrecht@gmail.com>
2022-08-20 15:18:53 -0500

    index rendering

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -5,11 +5,8 @@ const Items = require('../models/items.js')
 // INDEX
 // ASYNC & AWAIT
 router.get('/', async (req, res) => {
-    console.log('index')
 	let items = await Items.find({});
-    console.log('items', items);
-	// console.log('fruits', fruits);
-	res.send('index working')
+	res.render('index.ejs', { items });
 });
 
 module.exports = router
