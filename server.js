3070826ee1bcbd1353d916eb264084e122708653
pgarbrecht <philgarbrecht@gmail.com>
2022-08-20 13:23:03 -0500

    mongo and mongoose set up

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const Items = require('./models/items.js');
 require('dotenv').config()
 const PORT = process.env.PORT
 
+//Set up Mongoose
+const mongoose = require('mongoose');
+const mongoURI = process.env.MONGODB_URI
+mongoose.connect(mongoURI);
+mongoose.connection.once('open', () => {
+	console.log('connected to mongo');
+});
+
 // Default
 app.get('/', (req, res) => {
 	res.send('server working');
