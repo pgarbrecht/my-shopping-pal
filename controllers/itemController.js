db52913090ff82a3b3ab51e6d3b2b7b063512d97
pgarbrecht <philgarbrecht@gmail.com>
2022-08-22 05:55:12 -0500

    create and delete working

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,11 +2,30 @@ const express = require('express');
 const router = express.Router()
 const Items = require('../models/items.js')
 
-// INDEX
-// ASYNC & AWAIT
+// INDEX - ASYNC & AWAIT
 router.get('/', async (req, res) => {
 	let items = await Items.find({});
 	res.render('index.ejs', { items });
 });
+
+// CREATE
+router.post('/', (req, res) => {
+	Items.create(req.body, (error, createdItem) => {
+		if (error) {
+			console.log('error', error);
+			res.send(error);
+		} else {
+			res.redirect('/items');
+		}
+	});
+});
+
+// DELETE
+router.delete('/:id', (req, res) => {
+	Items.findByIdAndRemove(req.params.id, (err, data)=> {
+		if(err) console.log(err)
+		res.redirect('/items')
+	})
+})
 
 module.exports = router
