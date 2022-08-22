13cc15ba2c453cd75ee3dfcd52632173989870ab
pgarbrecht <philgarbrecht@gmail.com>
2022-08-22 08:00:43 -0500

    mongodb atlas database connected

--- models/items.js
+++ models/items.js
@@ -1,9 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+//Connection for Mongodb Atlas
+mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		console.log(`MongoDB connected at ${db.host}:${db.port}`)
+	})
+	.catch((err) => console.log(err))
+
 // Schema for each item
 const itemSchema = new mongoose.Schema({
 	name: { type: String, required: true },
-	notes: { type: String, required: false, default: 'Select edit to add notes or update image for this item' },
-	image: { type: String, required: false, default: '/images/placeholder-item.png' },
+	notes: { type: String, required: false, default: 'Select edit to add notes for this item' },
 });
 
 // Model based on schema
